ef8608b88e684168c5e1b7d6536a8210fe706687
ashwinsaji2588 <ashwinsaji2588@gmail.com>
2024-05-13 23:03:37 +0530

    settings changed

--- qkd/settings.py
+++ qkd/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web-chat-using-qkd.onrender.com']
 
+CSRF_TRUSTED_ORIGINS = ['https://web-chat-using-qkd.onrender.com']
+
 
 # Application definition
 
